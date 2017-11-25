6d73548a24b001b6f888488e0636ba6c941ee59c
Marko Mets <marko.mets@ttu.ee>
2017-11-25 18:35:54 +0200

    Increase gas limit

--- app/src/main/java/com/authcoinandroid/service/contract/AuthcoinContractParams.java
+++ app/src/main/java/com/authcoinandroid/service/contract/AuthcoinContractParams.java
@@ -3,8 +3,9 @@ package com.authcoinandroid.service.contract;
 import java.math.BigDecimal;
 
 class AuthcoinContractParams {
-    static final String CONTRACT_ADDRESS = "afc30d83b70ff1fc1336a3568fb0c4b38fe0fe1e";
+    static final String AUTHCOIN_CONTRACT_ADDRESS = "afc30d83b70ff1fc1336a3568fb0c4b38fe0fe1e";
 
+    static final int FUNCTION_GAS_LIMIT = 1000000; // 1 million
     static final int GAS_LIMIT = 25000;
     static final int GAS_PRICE = 40;
     static final BigDecimal FEE_PER_KB = BigDecimal.valueOf(0.004);
