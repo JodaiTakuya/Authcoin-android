40f1f05ce50a99624f613989ea7bb776eddafbca
Marko Mets <marko.mets@ttu.ee>
2017-12-10 17:28:33 +0200

    Update contract address

--- app/src/main/java/com/authcoinandroid/service/contract/AuthcoinContractParams.java
+++ app/src/main/java/com/authcoinandroid/service/contract/AuthcoinContractParams.java
@@ -3,7 +3,7 @@ package com.authcoinandroid.service.contract;
 import java.math.BigDecimal;
 
 class AuthcoinContractParams {
-    static final String AUTHCOIN_CONTRACT_ADDRESS = "4cec509a02861283abdd20d9d0958f6f9c231d25";
+    static final String AUTHCOIN_CONTRACT_ADDRESS = "15cecf8b43c092b2083e0894f6d5c21a17033e73";
 
     static final int FUNCTION_GAS_LIMIT = 1000000;
     static final int GAS_LIMIT = 25000;
