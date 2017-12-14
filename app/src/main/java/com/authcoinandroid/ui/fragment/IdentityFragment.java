6e32b2d31745c151bdc7e61c6535c9950679ec9e
Gregor.Johannson <gregor.johannson@ttu.ee>
2017-12-15 00:03:04 +0200

    Add swipe-to-refresh for Identities eirList

--- app/src/main/java/com/authcoinandroid/ui/fragment/IdentityFragment.java
+++ app/src/main/java/com/authcoinandroid/ui/fragment/IdentityFragment.java
@@ -6,6 +6,7 @@ import android.content.ClipboardManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,28 +28,29 @@ import com.authcoinandroid.ui.AuthCoinApplication;
 import com.authcoinandroid.ui.activity.MainActivity;
 import com.authcoinandroid.ui.activity.WelcomeActivity;
 import com.authcoinandroid.ui.adapter.EirAdapter;
+import com.authcoinandroid.util.AndroidUtil;
+import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
-
-import com.authcoinandroid.util.AndroidUtil;
 import org.bitcoinj.wallet.UnreadableWalletException;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class IdentityFragment extends Fragment {
     private final static String LOG_TAG = "IdentityFragment";
-
     @BindView(R.id.iv_wallet_copy)
     ImageView walletAddressCopy;
-
     @BindView(R.id.tv_unspent_output)
     TextView unspentOutput;
-
+    @BindView(R.id.swipe_refresh_eirs)
+    SwipeRefreshLayout swipeRefreshLayout;
     @BindView(R.id.lv_eirs)
     ListView eirList;
+    private List<EntityIdentityRecord> eirs;
 
     public IdentityFragment() {
     }
@@ -85,6 +87,32 @@ public class IdentityFragment extends Fragment {
         attachWalletAddressToCopyImage();
         displayUnspentOutputAmount();
         populateEirList();
+
+        AuthCoinApplication application = (AuthCoinApplication) getActivity().getApplication();
+        IdentityService identityService = application.getIdentityService();
+
+        // TODO This could probably be optimized by not repopulating the whole list
+        swipeRefreshLayout.setOnRefreshListener(() -> Observable.fromIterable(eirs)
+                .flatMap(eir -> identityService.updateEirStatusFromBc(eir).toObservable())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DisposableObserver<Object>() {
+                    @Override
+                    public void onComplete() {
+                        swipeRefreshLayout.setRefreshing(false);
+                        populateEirList();
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(LOG_TAG, e.getMessage());
+                    }
+                }));
+
         return view;
     }
 
@@ -92,7 +120,9 @@ public class IdentityFragment extends Fragment {
         try {
             IdentityService identityService = ((AuthCoinApplication) getActivity().getApplication()).getIdentityService();
 
-            List<EntityIdentityRecord> eirs = identityService.getAll();
+            eirs = new ArrayList<>();
+            eirs.addAll(identityService.getAll());
+
             EirAdapter adapter = new EirAdapter(getContext(), eirs);
             eirList.setAdapter(adapter);
 
