e8712d1a23d6a55534960dd8013ad0280a091ccc
Gregor.Johannson <gregor.johannson@ttu.ee>
2017-12-17 17:48:41 +0200

    Initial code populating CR-s list for specific EIR. Update ListItem style.

---
+++ app/src/main/java/com/authcoinandroid/ui/adapter/ChallengeAdapter.java
@@ -0,0 +1,35 @@
+package com.authcoinandroid.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import com.authcoinandroid.R;
+import com.authcoinandroid.model.ChallengeRecord;
+
+import java.util.List;
+
+public class ChallengeAdapter extends ArrayAdapter<ChallengeRecord> {
+
+    public ChallengeAdapter(Context context, List<ChallengeRecord> challengeRecords) {
+        super(context, 0, challengeRecords);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ChallengeRecord challengeRecord = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.custom_list_item, parent, false);
+        }
+        TextView challengeType = (TextView) convertView.findViewById(R.id.tv_title);
+        challengeType.setTag(position);
+        challengeType.setText(challengeRecord.getType());
+
+        TextView challengeTimestamp = (TextView) convertView.findViewById(R.id.tv_subtitle);
+        challengeTimestamp.setTag(position);
+        challengeTimestamp.setText(String.valueOf(challengeRecord.getTimestamp()));
+        return convertView;
+    }
+}
--- app/src/main/java/com/authcoinandroid/ui/fragment/ChallengeFragment.java
+++ app/src/main/java/com/authcoinandroid/ui/fragment/ChallengeFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,6 +18,7 @@ import com.authcoinandroid.model.ChallengeRecord;
 import com.authcoinandroid.model.EntityIdentityRecord;
 import com.authcoinandroid.ui.AuthCoinApplication;
 import com.authcoinandroid.ui.activity.MainActivity;
+import com.authcoinandroid.ui.adapter.ChallengeAdapter;
 import com.authcoinandroid.util.AndroidUtil;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.observers.DisposableObserver;
@@ -31,21 +33,13 @@ public class ChallengeFragment extends Fragment {
     private final static String LOG_TAG = "ChallengeFragment";
 
     @BindView(R.id.s_eir)
-    Spinner currentEirSpinner;
+    Spinner currentEir;
     @BindView(R.id.btn_new_challenge)
-
     FloatingActionButton newChallengeButton;
+    @BindView(R.id.lv_challenges)
+    ListView challengeList;
 
-    private AdapterView.OnItemSelectedListener eirSpinnerListener = new AdapterView.OnItemSelectedListener() {
-        @Override
-        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-            getChallengeRecordsForEir((EntityIdentityRecord) parent.getItemAtPosition(position));
-        }
-
-        @Override
-        public void onNothingSelected(AdapterView<?> parent) {
-        }
-    };
+    private List<ChallengeRecord> challengeRecords;
 
     public ChallengeFragment() {
     }
@@ -63,14 +57,13 @@ public class ChallengeFragment extends Fragment {
         AuthCoinApplication application = (AuthCoinApplication) getActivity().getApplication();
         List<EntityIdentityRecord> minedIdentities = application.getEirRepository().findByStatus(MINED);
         newChallengeButton.setOnClickListener(addChallengeListener(!minedIdentities.isEmpty()));
-        currentEirSpinner.setEnabled(!minedIdentities.isEmpty());
+        currentEir.setEnabled(!minedIdentities.isEmpty());
 
         // Uses the toString method of EntityIdentityRecord to display value in ArrayAdapter
-        ArrayAdapter<EntityIdentityRecord> dataAdapter = new ArrayAdapter<>(this.getActivity(), R.layout.spinner_item_light, minedIdentities);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        currentEirSpinner.setAdapter(dataAdapter);
-
-        currentEirSpinner.setOnItemSelectedListener(eirSpinnerListener);
+        ArrayAdapter<EntityIdentityRecord> currentEirAdapter = new ArrayAdapter<>(this.getActivity(), R.layout.spinner_item_light, minedIdentities);
+        currentEirAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        currentEir.setAdapter(currentEirAdapter);
+        currentEir.setOnItemSelectedListener(changeCurrentEirListener());
 
         return view;
     }
@@ -85,8 +78,45 @@ public class ChallengeFragment extends Fragment {
         };
     }
 
+    private AdapterView.OnItemSelectedListener changeCurrentEirListener() {
+        return new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                populateChallengeList((EntityIdentityRecord) parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        };
+    }
+
+    private void populateChallengeList(EntityIdentityRecord currentEir) {
+        try {
+            getChallengeRecordsForEir(currentEir);
+
+            if (challengeRecords != null) {
+                ChallengeAdapter adapter = new ChallengeAdapter(getContext(), challengeRecords);
+                challengeList.setAdapter(adapter);
+
+                challengeList.setOnItemClickListener((parent, view, position, id) -> {
+                    ChallengeRecord challengeRecord = challengeRecords.get(position);
+                    Bundle bundle = new Bundle();
+                    bundle.putByteArray("challengeRecord", challengeRecord.getId());
+
+                    if (getContext() instanceof MainActivity) {
+                        // TODO Open a new Fragment (perhaps separate one required for sent/received challenges?)
+                        // ((MainActivity) getContext()).applyFullFragmentWithBundle(/* CrFragment */, bundle);
+                    }
+                });
+            }
+        } catch (Exception e) {
+            Log.d(LOG_TAG, e.getMessage());
+        }
+    }
+
     private void getChallengeRecordsForEir(EntityIdentityRecord eir) {
-        List<ChallengeRecord> allChallengeRecords = new ArrayList<>();
+        List<ChallengeRecord> newChallengeRecords = new ArrayList<>();
 
         ((AuthCoinApplication) getActivity().getApplication()).getChallengeService()
                 .getChallengeRecordsForEir(eir)
@@ -95,12 +125,13 @@ public class ChallengeFragment extends Fragment {
                 .subscribe(new DisposableObserver<List<ChallengeRecord>>() {
                     @Override
                     public void onComplete() {
-                        // TODO populate the ListView with allChallengeRecords
+                        challengeRecords = new ArrayList<>();
+                        challengeRecords.addAll(newChallengeRecords);
                     }
 
                     @Override
-                    public void onNext(List<ChallengeRecord> challengeRecords) {
-                        allChallengeRecords.addAll(challengeRecords);
+                    public void onNext(List<ChallengeRecord> nextChallengeRecords) {
+                        newChallengeRecords.addAll(nextChallengeRecords);
                     }
 
                     @Override
